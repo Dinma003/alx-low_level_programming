ec05ee7c9298f3f3e1dce9cbf551658de8a7f2a8
Dinma003 <dinmaraymond@gmail.com>
2022-09-11 12:59:37 -0700

    Hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+# Include <stdio.h>
+
+/**
+* main - print the string in the put functiin
+*
+* this program prints "Programming is like building a multilingual puzzle
+* Return :0
+*/
+Int main(void)
+{
+       printf("with proper grammer,but the outcome is a piece of art,\n");
+       return(#);
+
