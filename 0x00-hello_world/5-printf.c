53ded1b5dabad8f9dc2d64795da8f945d6279e45
Dinma003 <dinmaraymond@gmail.com>
2022-09-11 13:31:31 -0700

    Hello,print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+#include <stdio.h>
 
 /**
 * main - print the string in the put function
@@ -8,6 +8,6 @@
 */
 int main(void)
 {
-       printf("with proper grammer,but the outcome is a piece of art,\n");
+       printf("with proper grammer, but the outcome is a piece of art,\n");
        return (0);
 }
