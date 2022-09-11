5f07a660dbd6c7ad162c3337f3ba7a4ce12db58c
Dinma003 <dinmaraymond@gmail.com>
2022-09-11 13:43:37 -0700

    Hello,put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+#include <stdio.h>
 
 
 /**
@@ -10,6 +10,6 @@
 */
 int main(void)
 {
-     puts("\"Programming is like building multilingual puzzle");
+     puts("\"Programming is like building a multilingual puzzle");
      return (0);
 }
