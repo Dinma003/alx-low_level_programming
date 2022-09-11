0af40ad8df29c9137b3dcafa9fb4815e15bdf7d1
Dinma003 <dinmaraymond@gmail.com>
2022-09-11 02:10:10 -0700

    Hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+*main - print  the string in the put function
+*
+* Description: using the maun function
+* this program prints "Programming is like building a multilingual puzzle
+* Return:o
+int main(void)
+{
+  puts c*\"Programming is likr building a multilingual puzzle");
+}
